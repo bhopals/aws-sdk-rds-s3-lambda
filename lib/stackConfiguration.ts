e8089d398d32607a2d49b543e3c8cd6be223fdbc
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-08-16 08:13:23 -0400

    S3 to be public bucket

--- lib/stackConfiguration.ts
+++ lib/stackConfiguration.ts
@@ -47,3 +47,4 @@ export const S3_PRINCIPAL = "*";
 export const S3_GET_OBJECT = "s3:GetObject";
 export const S3_PUT_OBJECT = "s3:PutObject";
 export const S3_DELETE_OBJECT = "s3:DeleteObject";
+export const S3_PUBLIC_BUCKET = "s3:Public";
