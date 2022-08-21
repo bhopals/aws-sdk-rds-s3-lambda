d26fd7dbcc592c3f4fbd83a34e0ae12d490c87dd
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-08-21 08:32:59 -0400

    refactor

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -98,7 +98,7 @@ export class CdkStarterStack extends cdk.Stack {
       value: rdsFnUrl.url,
     });
 
-    // DISABLE VPC CREATION
+    // DISABLE VPC CREATION and SG Creation
     /**** CREATE VPC */
     // const vpc = this.createVPC();
 
